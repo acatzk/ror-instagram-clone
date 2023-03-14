bc5a0bc65a10d2600b28153d9150db6cf0bf4249
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2023-03-14 08:16:20 +0800

    [Instagram-245] - [BE/FE] Implement Post Validation Error Messages (#3)
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :keywords, presence: true, length: { minimum: 5, maximum: 100 }
 end
